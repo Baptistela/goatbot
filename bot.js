2873d5969352592ce62721b53445b5bfecc815c9
Baptistela <34238886+Baptistela@users.noreply.github.com>
2017-12-04 12:21:08 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,52 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+const PREFIX = "$";
+const fs = require('fs');
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+//
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+      let eventFunction = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+  bot.on(eventName, (...args) => eventFunction.run(bot, ...args));
+    });
+  });
+  bot.on("message", message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+   
+    let command = message.content.split(" ")[0];
+    command = command.slice(PREFIX.length);
+   
+    let args = message.content.split(" ").slice(1);
+    // The list of if/else is replaced with those simple 2 lines:
+   
+    try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(bot, message, args);
+    } catch (err) {
+      console.error(err);
+    }
+   
+  }); 
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 
-// THIS  MUST  BE  THIS  WAY
+//
+bot.on("ready", () =>   {
+   console.log("BOT ONLINE")
+   console.log("----------")
+   });
+//
+bot.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find('name', 'welcome');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Bem Vindo ao Servidor, ${member}`);
+  });
+//
+
+// // //
 client.login(process.env.BOT_TOKEN);
